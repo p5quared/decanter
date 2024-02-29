23acdc693ef08252f063711e07b87b61fbacb1a8
Peter Vaiciulis <petervaiciulis@gmail.com>
2024-02-29 10:16:58 -0500

    Reorganize
    Move a lot of stuff out of main

---
+++ display.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sort"
+	"time"
+
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+	"github.com/p5quared/decanter/Autolab"
+	"golang.org/x/oauth2"
+)
+
+func displayUserInfo(user Autolab.UserResponse) {
+	var emph = lipgloss.NewStyle().
+		PaddingLeft(1).
+		Foreground(colorPrimary).
+		Bold(true)
+
+	var body = lipgloss.NewStyle().
+		Foreground(colorTextPrimary)
+
+	// UB Autolab API doesn't appear to return anything else for now.
+	// Could just be because I'm a transfer and have less info though...
+	s1 := fmt.Sprintf("%s: %s %s", emph.Render("Name"), body.Render(user.FirstName), body.Render(user.LastName))
+	s2 := fmt.Sprintf("%s: %s", emph.Render("Email"), body.Render(user.Email))
+	fmt.Println()
+	fmt.Println(s1)
+	fmt.Println(s2)
+}
+
+func displayCourseList(courses []Autolab.CoursesResponse) {
+	var headerStyle = lipgloss.NewStyle().
+		Align(lipgloss.Left).
+		Bold(true).
+		Foreground(colorPrimary).
+		PaddingTop(1).
+		PaddingLeft(0)
+	fmt.Println(headerStyle.Render("Your Current Courses (s24):"))
+
+	var courseStyle = lipgloss.NewStyle().
+		Align(lipgloss.Left).
+		PaddingLeft(1)
+
+	// TODO: Push an align course.Name to right
+	for _, course := range courses {
+		s := fmt.Sprintf("%s    (%s)", course.DisplayName, course.Name)
+		fmt.Println(courseStyle.Render(s))
+	}
+
+	var helpStyle = lipgloss.NewStyle().
+		Foreground(colorTextSubtle).PaddingLeft(1)
+
+	helpStr := "[Course Name]               (Course ID)"
+	fmt.Println(helpStyle.Render(helpStr))
+}
+
+func displayAssessmentList(httpClient *http.Client, courses []Autolab.CoursesResponse) {
+	var headerStyle = lipgloss.NewStyle().
+		Align(lipgloss.Left).
+		Bold(true).
+		Foreground(colorPrimary).
+		PaddingTop(1).
+		PaddingLeft(0)
+	fmt.Println(headerStyle.Render("Your Assessments:"))
+
+	var OddRowStyle = lipgloss.NewStyle().
+		Align(lipgloss.Left).
+		Foreground(colorTextPrimary)
+
+	var EvenRowStyle = lipgloss.NewStyle().
+		Inherit(OddRowStyle).
+		Foreground(colorTextSubtle)
+
+	t := table.New().
+		Headers("Course", "Assessment", "Assigned", "Due Date", "Closed").
+		Border(lipgloss.NormalBorder()).
+		StyleFunc(func(r, c int) lipgloss.Style {
+			switch {
+			case r == 0:
+				return lipgloss.NewStyle().Bold(true).Foreground(colorPrimary)
+			case r%2 == 0:
+				return EvenRowStyle
+			default:
+				return OddRowStyle
+			}
+		})
+
+	now := time.Now()
+	var assessmentsFetched int
+	for _, course := range courses {
+		assessments, _ := Autolab.GetUserAssessments(httpClient, host, course.Name)
+		for _, ass := range assessments {
+			assessmentsFetched++
+			// TODO: Parse dates
+			t.Row(course.Name, ass.Name, ass.Assigned[5:10], ass.Due[5:10], ass.Closed[5:10])
+		}
+	}
+	elapsed := time.Since(now)
+
+	fmt.Println(t.Render())
+
+	var helpStyle = lipgloss.NewStyle().
+		Faint(true).
+		Foreground(colorTextSubtle)
+	// Not actually true, as we fetched the courses earlier and got assignments now.
+	s := fmt.Sprintf("Fetched and processed %d courses and %d assessments in %v", len(courses), assessmentsFetched, elapsed.Truncate(time.Millisecond))
+	fmt.Println(helpStyle.Render(s))
+}
+
+func displaySubmission(submission Autolab.SubmissionsResponse) {
+	title := emph("Latest Submission")
+	fmt.Println(title)
+	fmt.Println(" Version: ", submission.Version)
+	fmt.Println(" Submitted: ", submission.Submitted)
+	fmt.Println(" Filename: ", submission.Filename)
+
+	var OddRowStyle = lipgloss.NewStyle().
+		Align(lipgloss.Left).
+		Foreground(colorTextPrimary)
+
+	var EvenRowStyle = lipgloss.NewStyle().
+		Inherit(OddRowStyle).
+		Foreground(colorTextSubtle)
+
+	t := table.New().
+		Headers("Problem", "Score").
+		Border(lipgloss.NormalBorder()).
+		StyleFunc(func(r, c int) lipgloss.Style {
+			switch {
+			case r == 0:
+				return lipgloss.NewStyle().Bold(true).Foreground(colorPrimary)
+			case r%2 == 0:
+				return EvenRowStyle
+			default:
+				return OddRowStyle
+			}
+		})
+	scores := submission.Scores
+	keys := make([]string, 0, len(scores))
+	for k := range scores {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		t.Row(k, fmt.Sprintf("%.2f", scores[k]))
+	}
+	fmt.Println(t.Render())
+}
+
+// Complete device flow and cache token to disk
+// TODO: Should ask to abort if token already exists
+func interactiveSetup(authClient Autolab.AutolabOAuthClient, fs Autolab.TokenStore) {
+	// 1. DeviceAuth
+	// 2. DeviceAccessCode
+	// 3. Exchange
+	// 4. Save
+	// NOTE: Error on anything is fatal for now.
+
+	var (
+		dResp oauth2.DeviceAuthResponse
+		dCode string
+		token oauth2.Token
+		err   error
+	)
+
+	spinner.New().
+		Title("Initiating device flow...").
+		Style(spinStyle).
+		Action(func() {
+			dResp, err = authClient.DeviceAuth()
+		}).Run()
+	if err != nil {
+		fmt.Println("Error starting device flow: ", err)
+		return
+	}
+	fmt.Println(finished("Starting device flow..."))
+	fmt.Printf("\n    Visit %s and enter the code %s to authenticate.\n\n", url(dResp.VerificationURI), emph(dResp.UserCode))
+
+	spinner.New().
+		Title(" Polling server to for access code...").
+		Type(spinner.Dots).
+		Style(spinStyle).
+		Action(func() {
+			dCode, err = authClient.DeviceAccessCode(dResp)
+		}).Run()
+	if err != nil || dCode == "" {
+		fmt.Println("Error getting code: ", err)
+		return
+	}
+	fmt.Println(finished("Got code"))
+
+	spinner.New().
+		Title("Exchanging code for token...").
+		Style(spinStyle).
+		Action(func() {
+			token, err = authClient.ExchangeCodeForToken(dCode)
+		}).Run()
+	if err != nil {
+		fmt.Println("Error exchanging code for token: ", err)
+		return
+	}
+	fmt.Println(finished("Exchanged code for token"))
+
+	spinner.New().
+		Title("Saving credentials...").
+		Style(spinStyle).
+		Action(func() {
+			err = fs.Save(token)
+		}).Run()
+	if err != nil {
+		fmt.Println("Error saving token: ", err)
+		return
+	}
+	fmt.Println(finished("Saved token"))
+
+	finStyle := lipgloss.NewStyle().
+		Foreground(colorPrimary).Bold(true).
+		PaddingTop(1).PaddingLeft(2)
+	fmt.Println(finStyle.Render("Decanter setup complete!"))
+}
---
+++ utils.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"time"
+
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/p5quared/decanter/Autolab"
+)
+
+func filter[T any](elements []T, p func(T) bool) []T {
+	var f []T
+	for _, item := range elements {
+		if p(item) {
+			f = append(f, item)
+		}
+	}
+	return f
+}
+
+func filterAssmts(assmts []Autolab.AssessmentsResponse, filter func(Autolab.AssessmentsResponse) bool) []Autolab.AssessmentsResponse {
+	var filtered []Autolab.AssessmentsResponse
+	for _, assmt := range assmts {
+		if filter(assmt) {
+			filtered = append(filtered, assmt)
+		}
+	}
+	return filtered
+}
+
+func filterCourses(c []Autolab.CoursesResponse, filter func(Autolab.CoursesResponse) bool) []Autolab.CoursesResponse {
+	var filtered []Autolab.CoursesResponse
+	for _, course := range c {
+		if filter(course) {
+			filtered = append(filtered, course)
+		}
+	}
+	return filtered
+}
+
+func testSpinner(t spinner.Type) {
+	spinner.New().
+		Title("Example spinner!").
+		Type(t).
+		Action(func() {
+			time.Sleep(5 * time.Second)
+		}).Run()
+}
