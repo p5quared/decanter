0178f9f06a124eb8d7fc11e1523b4e4d6d654a77
Peter Vaiciulis <petervaiciulis@gmail.com>
2024-03-01 22:52:37 -0500

    new time formats

--- display.go
+++ display.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"sort"
-	"time"
 
 	"github.com/charmbracelet/huh/spinner"
 	"github.com/charmbracelet/lipgloss"
@@ -88,33 +87,45 @@ func displayAssessmentList(httpClient *http.Client, courses []Autolab.CoursesRes
 			}
 		})
 
-	now := time.Now()
 	var assessmentsFetched int
 	for _, course := range courses {
 		assessments, _ := Autolab.GetUserAssessments(httpClient, host, course.Name)
 		for _, ass := range assessments {
 			assessmentsFetched++
-			// TODO: Parse dates
-			t.Row(course.Name, ass.Name, ass.Assigned[5:10], ass.Due[5:10], ass.Closed[5:10])
+
+			// TODO: Align columns
+			assTime := func(time_raw string) string {
+				tt := Autolab.ParseTime(time_raw)
+				return fmt.Sprintf("%s %d-%d", tt.Weekday().String()[:3], tt.Month(), tt.Day())
+			}
+
+			t.Row(course.Name, ass.Name, assTime(ass.Assigned), assTime(ass.Due), assTime(ass.Closed))
 		}
 	}
-	elapsed := time.Since(now)
-
 	fmt.Println(t.Render())
+}
 
-	var helpStyle = lipgloss.NewStyle().
-		Faint(true).
-		Foreground(colorTextSubtle)
-	// Not actually true, as we fetched the courses earlier and got assignments now.
-	s := fmt.Sprintf("Fetched and processed %d courses and %d assessments in %v", len(courses), assessmentsFetched, elapsed.Truncate(time.Millisecond))
-	fmt.Println(helpStyle.Render(s))
+func displayTime(time_raw string) string {
+	t := Autolab.ParseTime(time_raw)
+	tSuffix := "AM"
+	if t.Hour() >= 12 {
+		tSuffix = "PM"
+	}
+
+	twelveHour := t.Hour()
+	if twelveHour > 12 {
+		twelveHour -= 12
+	}
+
+	return fmt.Sprintf("%s %d-%d (%d:%d%s)", t.Weekday(), t.Month(), t.Day(), twelveHour, t.Minute(), tSuffix)
 }
 
 func displaySubmission(submission Autolab.SubmissionsResponse) {
+
 	title := emph("Latest Submission")
 	fmt.Println(title)
 	fmt.Println(" Version: ", submission.Version)
-	fmt.Println(" Submitted: ", submission.Submitted)
+	fmt.Println(" Submitted: ", displayTime(submission.Submitted))
 	fmt.Println(" Filename: ", submission.Filename)
 
 	var OddRowStyle = lipgloss.NewStyle().
